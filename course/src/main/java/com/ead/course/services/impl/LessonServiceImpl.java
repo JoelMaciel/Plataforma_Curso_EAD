86b3bfeb6d62d5510f497fb70cf3f3c6f363ce66
Joel Maciel <soldado25461@gmail.com>
2022-09-06 14:41:15 -0300

    Deleção de Relacionamento Cascade.ALL ou Método de Deleção Customizado

--- course/src/main/java/com/ead/course/services/impl/LessonServiceImpl.java
+++ course/src/main/java/com/ead/course/services/impl/LessonServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class LessoServiceImpl implements LessonService {
+public class LessonServiceImpl implements LessonService {
 
     @Autowired
     private LessonRepository lessonRepository;
