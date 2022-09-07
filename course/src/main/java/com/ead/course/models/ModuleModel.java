ce4e68b11a0342961e1fd5c89a495914df4a97b8
Joel Maciel <soldado25461@gmail.com>
2022-09-06 21:10:51 -0300

    Criar API RESTful para Course com Validação Spring Validation

--- course/src/main/java/com/ead/course/models/ModuleModel.java
+++ course/src/main/java/com/ead/course/models/ModuleModel.java
@@ -5,6 +5,8 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
 import java.io.Serializable;
@@ -33,11 +35,13 @@ public class ModuleModel implements Serializable {
     @Column(nullable = false)
     private LocalDateTime creationDate;
 
+
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @ManyToOne(optional = false , fetch = FetchType.LAZY)
     private CourseModel course;
 
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     @OneToMany(mappedBy = "module" ,fetch = FetchType.LAZY)
+    @Fetch(FetchMode.SUBSELECT)
     private Set<LessonModel> lessons;
 }
