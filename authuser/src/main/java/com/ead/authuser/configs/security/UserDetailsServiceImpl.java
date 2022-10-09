579806429e592dc9d0c2d24a17008c81f7175d69
Joel Maciel <soldado25461@gmail.com>
2022-10-09 15:31:51 -0300

    Generate JWT com Subject e Roles

--- authuser/src/main/java/com/ead/authuser/configs/security/UserDetailsServiceImpl.java
+++ authuser/src/main/java/com/ead/authuser/configs/security/UserDetailsServiceImpl.java
@@ -2,10 +2,13 @@ package com.ead.authuser.configs.security;
 
 import com.ead.authuser.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -17,6 +20,12 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         var userModel = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User Not Found with userame: " + username));
+        return UserDetailsImpl.build(userModel);
+    }
+
+    public UserDetails loadUserById(UUID userId) throws AuthenticationCredentialsNotFoundException {
+        var userModel = userRepository.findById(userId)
+                .orElseThrow(() -> new AuthenticationCredentialsNotFoundException("User Not Found with userame: " + userId));
         return UserDetailsImpl.build(userModel);
     }
 }
