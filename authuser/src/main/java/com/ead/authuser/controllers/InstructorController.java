2c28c34097fdf48103a57f6d8d71d30472c96ac1
Joel Maciel <soldado25461@gmail.com>
2022-10-10 19:43:40 -0300

    Authorization Roles para Instructor

--- authuser/src/main/java/com/ead/authuser/controllers/InstructorController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/InstructorController.java
@@ -1,12 +1,15 @@
 package com.ead.authuser.controllers;
 
 import com.ead.authuser.dtos.InstructorDto;
+import com.ead.authuser.enums.RoleType;
 import com.ead.authuser.enums.UserType;
 import com.ead.authuser.models.UserModel;
+import com.ead.authuser.services.RoleService;
 import com.ead.authuser.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -23,15 +26,22 @@ public class InstructorController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private RoleService roleService;
+
+    @PreAuthorize("hasAnyRole('ADMIN')")
     @PostMapping("/subscription")
     public ResponseEntity<Object> saveSubscriptionInstructor(@RequestBody @Valid InstructorDto instructorDto) {
         Optional<UserModel> userModelOptional = userService.findById(instructorDto.getUserId());
         if(!userModelOptional.isPresent()) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("User not found.");
         }else {
+            var roleModel = roleService.findByRoleName(RoleType.ROLE_INSTRUCTOR)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found"));
             var userModel = userModelOptional.get();
             userModel.setUserType(UserType.INSTRUCTOR);
             userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
+            userModel.getRoles().add(roleModel);
             userService.updateUser(userModel);
             return  ResponseEntity.status(HttpStatus.CREATED).body(userModel);
         }
